97bea8a76c45fbe5ed3dcd4f5188d2340b1ad571
Ben Paddock <pads@thisispads.me.uk>
2013-01-19 15:04:59 +0000

    Include RequireJS text plugin

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,7 @@
 require.config({
     baseUrl: "/src/js",
     paths: {
+        text: "lib/text",
         jquery: "lib/jquery",
         underscore: "lib/underscore",
         backbone: "lib/backbone"
