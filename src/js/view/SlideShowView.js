eb755f8672202b1139089730024b705ba9613af3
Ben Paddock <pads@thisispads.me.uk>
2013-01-27 22:00:29 +0000

    Navigate with the arrow keys plus fullscreen option

--- src/js/view/SlideShowView.js
+++ src/js/view/SlideShowView.js
@@ -5,6 +5,12 @@ define([
     "text!/src/html/templates/slideshow.html"
 ], function (Backbone, _, $, slideShowView) {
 
+    var KEY_ENTER = 13;
+    var KEY_LEFT = 37;
+    var KEY_UP = 38;
+    var KEY_RIGHT = 39;
+    var KEY_DOWN = 40;
+
     var slideCounter = 0;
 
     return Backbone.View.extend({
@@ -15,7 +21,12 @@ define([
             "click #firstslide": "firstSlide",
             "click #nextslide": "nextSlide",
             "click #previousslide": "previousSlide",
-            "click #lastslide": "lastSlide"
+            "click #lastslide": "lastSlide",
+            "click #fullscreen": "fullScreen"
+        },
+        initialize: function () {
+            _.bindAll(this, "navigate");
+            document.onkeydown = this.navigate;
         },
         render: function (index) {
             if (!index) {
@@ -39,6 +50,32 @@ define([
         lastSlide: function () {
             slideCounter = this.collection.length - 1;
             this.render(slideCounter);
+        },
+        fullScreen: function () {
+            if (this.el.webkitRequestFullscreen) {
+                this.el.webkitRequestFullscreen();
+            } else if (this.el.mozRequestFullScreen) {
+                this.el.mozRequestFullScreen();
+            }
+        },
+        navigate: function (event) {
+            switch (event.keyCode) {
+            case KEY_ENTER:
+                this.fullScreen();
+                break;
+            case KEY_LEFT:
+                this.previousSlide();
+                break;
+            case KEY_UP:
+                this.lastSlide();
+                break;
+            case KEY_RIGHT:
+                this.nextSlide();
+                break;
+            case KEY_DOWN:
+                this.firstSlide();
+                break;
+            }
         }
     });
 });
